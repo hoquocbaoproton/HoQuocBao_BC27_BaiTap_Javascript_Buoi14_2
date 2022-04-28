4edc239d994a64b5ea57b7f7563f0f565c509e94
quocBao <hoquocbao@protonmail.com>
2022-04-28 22:29:15 +0700

    fix bad code

--- ex5.js
+++ ex5.js
@@ -39,21 +39,13 @@ const prevDate = function () {
   let year = +document.getElementById('yearEx5').value;
   const exportTxtEx5 = document.getElementById('exportTxtEx5');
 
-  let checkYear;
-  if (year < 1) {
-    checkYear = 'Trước công nguyên';
-  } else {
-    checkYear = 'Sau công nguyên';
-  }
+  let checkYear = year < 1 ? 'Trước công nguyên' : 'Sau công nguyên';
 
   const decreaseDate = function (date, month, year) {
     if (date === 1) {
       date = 31;
-      if (month === 1) {
-        month = 12;
-      } else {
-        month -= 1;
-      }
+
+      month === 1 ? (month = 12) : (month -= 1);
       exportTxtEx5.style.backgroundColor = '#fff';
       exportTxtEx5.textContent = `Ngày tháng năm trước đó là: ${date} / ${month} / ${year} ${checkYear}`;
     } else {
@@ -67,43 +59,44 @@ const prevDate = function () {
     exportTxtEx5.style.backgroundColor = '#ffc107';
     exportTxtEx5.textContent =
       'ngày tháng phải là số nguyên dương hợp lệ, năm < 0 là trước công nguyên, >= 0  là sau công nguyên';
-  } else {
-    switch (month) {
-      case 1:
-        year -= 1;
+    return;
+  }
+  switch (month) {
+    case 1:
+      year -= 1;
+      decreaseDate(date, month, year);
+      break;
+    case 2:
+      if (date > 28) {
+        exportTxtEx5.style.backgroundColor = '#ffc107';
+        exportTxtEx5.textContent = 'tháng 2 chỉ có 28 ngày';
+      } else {
         decreaseDate(date, month, year);
-        break;
-      case 2:
-        if (date > 28) {
-          exportTxtEx5.style.backgroundColor = '#ffc107';
-          exportTxtEx5.textContent = 'tháng 2 chỉ có 28 ngày';
-        } else {
-          decreaseDate(date, month, year);
-        }
-        break;
-      case 3:
-      case 5:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
+      }
+      break;
+    case 3:
+    case 5:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      decreaseDate(date, month, year);
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      if (date > 30) {
+        exportTxtEx5.style.backgroundColor = '#ffc107';
+        exportTxtEx5.textContent = `tháng ${month} chỉ có 30 ngày`;
+      } else {
         decreaseDate(date, month, year);
-        break;
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-        if (date > 30) {
-          exportTxtEx5.style.backgroundColor = '#ffc107';
-          exportTxtEx5.textContent = `tháng ${month} chỉ có 30 ngày`;
-        } else {
-          decreaseDate(date, month, year);
-        }
-        break;
-    }
+      }
+      break;
   }
 };
+//};
 
 const nextDate = function () {
   let date = +document.getElementById('dateEx5').value;
@@ -121,11 +114,7 @@ const nextDate = function () {
   const increaseDate = function (date, month, year) {
     if (date === 31) {
       date = 1;
-      if (month === 12) {
-        month = 1;
-      } else {
-        month += 1;
-      }
+      month === 12 ? (month = 1) : (month += 1);
       exportTxtEx5.style.backgroundColor = '#fff';
       exportTxtEx5.textContent = `Ngày tháng năm sau đó là: ${date} / ${month} / ${year} ${checkYear}`;
     } else {
@@ -139,59 +128,60 @@ const nextDate = function () {
     exportTxtEx5.style.backgroundColor = '#ffc107';
     exportTxtEx5.textContent =
       'ngày tháng phải là số nguyên dương hợp lệ, năm < 0 là trước công nguyên, >= 0  là sau công nguyên';
-  } else {
-    switch (month) {
-      case 12:
-        if (date === 31) {
-          year += 1;
-          increaseDate(date, month, year);
-        } else {
-          increaseDate(date, month, year);
-        }
-        break;
-      case 2:
-        if (date === 28) {
-          date = 1;
-          month += 1;
-          exportTxtEx5.style.backgroundColor = '#fff';
-          exportTxtEx5.textContent = `Ngày tháng năm sau đó là: ${date} / ${month} / ${year} ${checkYear}`;
-        } else if (date > 28) {
-          exportTxtEx5.style.backgroundColor = '#ffc107';
-          exportTxtEx5.textContent = `tháng ${month} chỉ có 28 ngày`;
-        } else {
-          exportTxtEx5.style.backgroundColor = '#fff';
-          increaseDate(date, month, year);
-        }
-        break;
-      case 1:
-      case 3:
-      case 5:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
+    return;
+  } // else {
+  switch (month) {
+    case 12:
+      if (date === 31) {
+        year += 1;
         increaseDate(date, month, year);
-        break;
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-        if (date === 30) {
-          date = 1;
-          month += 1;
-          exportTxtEx5.style.backgroundColor = '#fff';
-          exportTxtEx5.textContent = `Ngày tháng năm sau đó là: ${date} / ${month} / ${year} ${checkYear}`;
-        } else if (date === 31) {
-          exportTxtEx5.style.backgroundColor = '#ffc107';
-          exportTxtEx5.textContent = `tháng ${month} chỉ có 30 ngày`;
-        } else {
-          exportTxtEx5.style.backgroundColor = '#fff';
-          increaseDate(date, month, year);
-        }
-        break;
-    }
+      } else {
+        increaseDate(date, month, year);
+      }
+      break;
+    case 2:
+      if (date === 28) {
+        date = 1;
+        month += 1;
+        exportTxtEx5.style.backgroundColor = '#fff';
+        exportTxtEx5.textContent = `Ngày tháng năm sau đó là: ${date} / ${month} / ${year} ${checkYear}`;
+      } else if (date > 28) {
+        exportTxtEx5.style.backgroundColor = '#ffc107';
+        exportTxtEx5.textContent = `tháng ${month} chỉ có 28 ngày`;
+      } else {
+        exportTxtEx5.style.backgroundColor = '#fff';
+        increaseDate(date, month, year);
+      }
+      break;
+    case 1:
+    case 3:
+    case 5:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+      increaseDate(date, month, year);
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      if (date === 30) {
+        date = 1;
+        month += 1;
+        exportTxtEx5.style.backgroundColor = '#fff';
+        exportTxtEx5.textContent = `Ngày tháng năm sau đó là: ${date} / ${month} / ${year} ${checkYear}`;
+      } else if (date === 31) {
+        exportTxtEx5.style.backgroundColor = '#ffc107';
+        exportTxtEx5.textContent = `tháng ${month} chỉ có 30 ngày`;
+      } else {
+        exportTxtEx5.style.backgroundColor = '#fff';
+        increaseDate(date, month, year);
+      }
+      break;
   }
 };
+//};
 
 prevDateBtn.addEventListener('click', prevDate);
 nextDateBtn.addEventListener('click', nextDate);
--- ex6.js
+++ ex6.js
@@ -22,45 +22,45 @@ const calcDate = function () {
   const year = +document.getElementById('yearEx6').value;
   const exportTxtEx6 = document.getElementById('exportTxtEx6');
 
-  let date;
-
-  const exportDate = function () {
+  const exportDate = function (date) {
+    exportTxtEx6.style.backgroundColor = '#fff';
     exportTxtEx6.textContent = `số ngày của tháng: ${date}`;
   };
+
+  let date;
 
   if (Number.isInteger(month, year) === false || month < 1 || month > 12) {
     exportTxtEx6.style.backgroundColor = '#ffc107';
     exportTxtEx6.textContent =
       'ngày tháng phải là số nguyên dương hợp lệ, năm < 0 là trước công nguyên, >= 0  là sau công nguyên';
-  } else {
-    exportTxtEx6.style.backgroundColor = '#fff';
-    switch (month) {
-      case 2:
-        if (year % 400 === 0 || (year % 4 !== 0 && year % 4 === 0)) {
-          date = 29;
-          exportDate(date);
-        } else {
-          date = 28;
-          exportDate(date);
-        }
-        break;
-      case 1:
-      case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
-        date = 31;
+    return;
+  }
+  switch (month) {
+    case 2:
+      if (year % 400 === 0 || (year % 4 !== 0 && year % 4 === 0)) {
+        date = 29;
         exportDate(date);
-        break;
-      case 4:
-      case 11:
-      case 9:
-      case 11:
-        date = 30;
+      } else {
+        date = 28;
         exportDate(date);
-    }
+      }
+      break;
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      date = 31;
+      exportDate(date);
+      break;
+    case 4:
+    case 11:
+    case 9:
+    case 11:
+      date = 30;
+      exportDate(date);
   }
 };
 
